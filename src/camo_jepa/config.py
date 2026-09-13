98a51322b60f44707f02866a5f23dca865c2e11b
Thanh Pham <thanhpham1108.work@gmail.com>
2026-09-13 22:36:55 +0700

    Change to batch size =4

--- src/camo_jepa/config.py
+++ src/camo_jepa/config.py
@@ -16,7 +16,7 @@ class CaMoJEPAConfig:
     stride: int = 1
     image_size: Tuple[int, int] = (512, 256)
     max_cached_episodes: int = 16
-    batch_size: int = 8  # Giảm xuống 8 để vừa khít 40GB VRAM (A100)
+    batch_size: int = 4  # Giảm xuống 8 để vừa khít 40GB VRAM (A100)
     shuffle: bool = True
     num_workers: int = 4
 
